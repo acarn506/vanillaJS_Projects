55649d3d73f697cc6d1ebe01bb3a95ade1c6c877
Anthony Carnero <37918263+acarn506@users.noreply.github.com>
2021-09-13 16:25:01 -0700

    menu items displayed

--- menu/app.js
+++ menu/app.js
@@ -72,3 +72,28 @@ const menu = [
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
   ];
+
+const sectionCenter = document.querySelector('.section-center')
+
+// load content
+window.addEventListener('DOMContentLoaded', () => {
+   displayMenuItems(menu)
+})
+
+function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map( item => {
+        return `<artclie class="menu-item">
+        <img src=${item.img}" alt="menu item" class="photo"/>
+        <div class="item-info">
+            <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}</h4>
+            </header>
+            <p class="item-text">${item.desc}</p>
+        </div>
+    </artclie>` 
+    })
+
+    displayMenu = displayMenu.join('')
+    sectionCenter.innerHTML = displayMenu
+}
