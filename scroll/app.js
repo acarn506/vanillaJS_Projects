8ccbe80adf51a81c5c882ac5f9c1f7a46897b491
Anthony Carnero <37918263+acarn506@users.noreply.github.com>
2021-09-22 13:39:52 -0700

    html structure added

--- scroll/app.js
+++ scroll/app.js
@@ -4,10 +4,12 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.getElementById('date')
+date.innerHTML = new Date().getFullYear()
 // ********** close links ************
 
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
 // select links
+
