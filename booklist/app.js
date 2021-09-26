49d0779bff42599ca5014b0f85f3cc02df959531
Anthony Carnero <37918263+acarn506@users.noreply.github.com>
2021-09-25 18:26:37 -0700

    add/remove functionality

--- booklist/app.js
+++ booklist/app.js
@@ -28,7 +28,7 @@ class UI {
     }
 
     static addBookToList(book) {
-        const list = document.querySelector('.book-list')
+        const list = document.querySelector('#book-list')
         const row = document.createElement('tr')
         row.innerHTML = `
             <td>${book.title}</td>
@@ -38,12 +38,48 @@ class UI {
         `
         list.appendChild(row)
     }
+
+    static clearFields() {
+        document.querySelector('#title').value = ''
+        document.querySelector('#author').value = ''
+        document.querySelector('#isbn').value = ''
+    }
+
+    static deleteBook(el) {
+        if (el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove()
+        }
+    }
 }
 
 // Store Class: Handles Storage
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
 // Event: Add a book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    // Prevent actual Submit
+    e.preventDefault()
+
+    // Get form values
+    const title = document.querySelector('#title').value
+    const author = document.querySelector('#author').value
+    const isbn = document.querySelector('#isbn').value
+
+    // Instatiate book instance
+    const book = new Book(title, author, isbn)
+
+    // Add book to UI
+    UI.addBookToList(book)
+
+    // UI Clear fields
+    UI.clearFields()
+
+})
 
 // Event: Remove a book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target)
+})
