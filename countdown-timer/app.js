54cf1b7939bb36834ac4fa1c67911ed8a3ebdb28
Anthony Carnero <37918263+acarn506@users.noreply.github.com>
2021-12-16 09:48:22 -0800

    counter init

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -21,3 +21,39 @@ const months = [
     "Friday",
     "Saturday",
   ];
+
+const giveaway = document.querySelector('.giveaway')
+const deadline = document.querySelector('.deadline')
+const items = document.querySelectorAll('.deadline-format h4')
+
+let futureDate = new Date(2021, 9, 24, 10, 30, 0)
+
+const year = futureDate.getFullYear()
+const hours = futureDate.getHours()
+const minutes = futureDate.getMinutes()
+const month = months[futureDate.getMonth()]
+const weekday = weekdays[futureDate.getDay()]
+const day = futureDate.getDate()
+
+giveaway.innerText = `Giveaway ends on ${weekday}, ${day} ${month} ${year} ${hours}:${minutes}am`
+
+const futureTime = futureDate.getTime()
+
+function getRemainingTime() {
+  const today = new Date().getTime()
+  const t = futureTime - today
+  console.log(t)
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000 
+  const oneHour = 60 * 60 * 1000
+  const oneMin = 60 * 1000
+  const oneSec = 1000
+
+  
+
+}
+
+getRemainingTime()
+
+  
